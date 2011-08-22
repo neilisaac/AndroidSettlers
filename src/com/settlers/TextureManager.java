a31a6594767b84df72e7b8c5f00f46f927bfbd68
Neil Isaac <android@neilisaac.ca>
2011-08-22 07:30:15 -0700

    Removed useless hash function calls in TextureManager

--- src/com/settlers/TextureManager.java
+++ src/com/settlers/TextureManager.java
@@ -15,7 +15,7 @@ import android.opengl.GLUtils;
 public class TextureManager {
 
 	private enum Type {
-		NONE, BACKGROUND, TILE, RESOURCE, TRADER, ROBBER, ROLL, ROAD, TOWN, CITY, ORNAMENT, BUTTONBG, BUTTON
+		NONE, BACKGROUND, SHORE, TILE, LIGHT, TRADER, RESOURCE, ROBBER, ROLL, ROAD, TOWN, CITY, ORNAMENT, BUTTONBG, BUTTON
 	}
 
 	public enum Location {
@@ -34,51 +34,18 @@ public class TextureManager {
 		return variant << 6 | type.ordinal();
 	}
 
-	private static int hash(Location location) {
-		return location.ordinal();
-	}
-
-	private static int hash(Trader.Position trader) {
-		return trader.ordinal();
-	}
-
-	private static int hash(Background background) {
-		return background.ordinal();
-	}
-
-	private static int hash(Hexagon.Type resource) {
-		return resource.ordinal();
-	}
-
-	private static int hash(UIButton.Background background) {
-		return background.ordinal();
-	}
-
-	private static int hash(UIButton.Type button) {
-		return button.ordinal();
-	}
-
-	private static int hash(Player.Color player) {
-		return player.ordinal();
-	}
-
 	private Bitmap get(Type type, int variant) {
 		return bitmap.get(hash(type, variant));
 	}
 
 	private void add(Type type, int variant, int id, Resources res) {
 		int key = hash(type, variant);
-		Bitmap bitmap = load(id, res);
+		Bitmap bitmap = BitmapFactory.decodeResource(res, id, new Options());
 		this.bitmap.put(key, bitmap);
 		this.resource.put(key, id);
-		this.square.put(key,
-				new Square(id, 0, 0, type.ordinal(), bitmap.getWidth()
-						/ Geometry.TILE_SIZE, bitmap.getHeight()
-						/ Geometry.TILE_SIZE));
-	}
-
-	private static Bitmap load(int id, Resources res) {
-		return BitmapFactory.decodeResource(res, id, new Options());
+		this.square.put(key, new Square(id, 0, 0, type.ordinal(),
+				(float) bitmap.getWidth() / (float) Geometry.TILE_SIZE,
+				(float) bitmap.getHeight() / (float) Geometry.TILE_SIZE));
 	}
 
 	public TextureManager(Resources res) {
@@ -87,20 +54,18 @@ public class TextureManager {
 		resource = new Hashtable<Integer, Integer>();
 		square = new Hashtable<Integer, Square>();
 
-		// load background
-		add(Type.BACKGROUND, hash(Background.WAVES),
-				R.drawable.background_waves, res);
-
 		// load large tile textures
-		add(Type.TILE, hash(Hexagon.Type.SHORE), R.drawable.tile_shore, res);
-		add(Type.TILE, hash(Hexagon.Type.DESERT), R.drawable.tile_desert, res);
-		add(Type.TILE, hash(Hexagon.Type.WOOL), R.drawable.tile_wool, res);
-		add(Type.TILE, hash(Hexagon.Type.GRAIN), R.drawable.tile_grain, res);
-		add(Type.TILE, hash(Hexagon.Type.LUMBER), R.drawable.tile_lumber, res);
-		add(Type.TILE, hash(Hexagon.Type.BRICK), R.drawable.tile_brick, res);
-		add(Type.TILE, hash(Hexagon.Type.ORE), R.drawable.tile_ore, res);
-		add(Type.TILE, hash(Hexagon.Type.DIM), R.drawable.tile_dim, res);
-		add(Type.TILE, hash(Hexagon.Type.LIGHT), R.drawable.tile_light, res);
+		add(Type.SHORE, 0, R.drawable.tile_shore, res);
+		add(Type.TILE, Hexagon.Type.DESERT.ordinal(), R.drawable.tile_desert,
+				res);
+		add(Type.TILE, Hexagon.Type.WOOL.ordinal(), R.drawable.tile_wool, res);
+		add(Type.TILE, Hexagon.Type.GRAIN.ordinal(), R.drawable.tile_grain, res);
+		add(Type.TILE, Hexagon.Type.LUMBER.ordinal(), R.drawable.tile_lumber,
+				res);
+		add(Type.TILE, Hexagon.Type.BRICK.ordinal(), R.drawable.tile_brick, res);
+		add(Type.TILE, Hexagon.Type.ORE.ordinal(), R.drawable.tile_ore, res);
+		add(Type.TILE, Hexagon.Type.DIM.ordinal(), R.drawable.tile_dim, res);
+		add(Type.LIGHT, 0, R.drawable.tile_light, res);
 
 		// load roll number textures
 		add(Type.ROLL, 2, R.drawable.roll_2, res);
@@ -118,71 +83,82 @@ public class TextureManager {
 		add(Type.ROBBER, 0, R.drawable.tile_robber, res);
 
 		// load button textures
-		add(Type.BUTTONBG, hash(UIButton.Background.BACKDROP),
+		add(Type.BUTTONBG, UIButton.Background.BACKDROP.ordinal(),
 				R.drawable.button_backdrop, res);
-		add(Type.BUTTONBG, hash(UIButton.Background.PRESSED),
+		add(Type.BUTTONBG, UIButton.Background.PRESSED.ordinal(),
 				R.drawable.button_press, res);
-		add(Type.BUTTON, hash(UIButton.Type.INFO), R.drawable.button_status,
+		add(Type.BUTTON, UIButton.Type.INFO.ordinal(),
+				R.drawable.button_status, res);
+		add(Type.BUTTON, UIButton.Type.ROLL.ordinal(), R.drawable.button_roll,
 				res);
-		add(Type.BUTTON, hash(UIButton.Type.ROLL), R.drawable.button_roll, res);
-		add(Type.BUTTON, hash(UIButton.Type.ROAD), R.drawable.button_road, res);
-		add(Type.BUTTON, hash(UIButton.Type.TOWN),
+		add(Type.BUTTON, UIButton.Type.ROAD.ordinal(), R.drawable.button_road,
+				res);
+		add(Type.BUTTON, UIButton.Type.TOWN.ordinal(),
 				R.drawable.button_settlement, res);
-		add(Type.BUTTON, hash(UIButton.Type.CITY), R.drawable.button_city, res);
-		add(Type.BUTTON, hash(UIButton.Type.DEVCARD),
+		add(Type.BUTTON, UIButton.Type.CITY.ordinal(), R.drawable.button_city,
+				res);
+		add(Type.BUTTON, UIButton.Type.DEVCARD.ordinal(),
 				R.drawable.button_development, res);
-		add(Type.BUTTON, hash(UIButton.Type.TRADE), R.drawable.button_trade,
-				res);
-		add(Type.BUTTON, hash(UIButton.Type.ENDTURN),
+		add(Type.BUTTON, UIButton.Type.TRADE.ordinal(),
+				R.drawable.button_trade, res);
+		add(Type.BUTTON, UIButton.Type.ENDTURN.ordinal(),
 				R.drawable.button_endturn, res);
-		add(Type.BUTTON, hash(UIButton.Type.CANCEL), R.drawable.button_cancel,
-				res);
+		add(Type.BUTTON, UIButton.Type.CANCEL.ordinal(),
+				R.drawable.button_cancel, res);
 
 		// load large town textures
-		add(Type.TOWN, hash(Player.Color.SELECT), R.drawable.settlement_purple,
+		add(Type.TOWN, Player.Color.SELECT.ordinal(),
+				R.drawable.settlement_purple, res);
+		add(Type.TOWN, Player.Color.RED.ordinal(), R.drawable.settlement_red,
 				res);
-		add(Type.TOWN, hash(Player.Color.RED), R.drawable.settlement_red, res);
-		add(Type.TOWN, hash(Player.Color.BLUE), R.drawable.settlement_blue, res);
-		add(Type.TOWN, hash(Player.Color.GREEN), R.drawable.settlement_green,
-				res);
-		add(Type.TOWN, hash(Player.Color.ORANGE), R.drawable.settlement_orange,
-				res);
+		add(Type.TOWN, Player.Color.BLUE.ordinal(),
+				R.drawable.settlement_green, res);
+		add(Type.TOWN, Player.Color.ORANGE.ordinal(),
+				R.drawable.settlement_orange, res);
 
 		// load large city textures
-		add(Type.CITY, hash(Player.Color.SELECT), R.drawable.city_purple, res);
-		add(Type.CITY, hash(Player.Color.RED), R.drawable.city_red, res);
-		add(Type.CITY, hash(Player.Color.BLUE), R.drawable.city_blue, res);
-		add(Type.CITY, hash(Player.Color.GREEN), R.drawable.city_green, res);
-		add(Type.CITY, hash(Player.Color.ORANGE), R.drawable.city_orange, res);
+		add(Type.CITY, Player.Color.SELECT.ordinal(), R.drawable.city_purple,
+				res);
+		add(Type.CITY, Player.Color.RED.ordinal(), R.drawable.city_red, res);
+		add(Type.CITY, Player.Color.BLUE.ordinal(), R.drawable.city_blue, res);
+		add(Type.CITY, Player.Color.GREEN.ordinal(), R.drawable.city_green, res);
+		add(Type.CITY, Player.Color.ORANGE.ordinal(), R.drawable.city_orange,
+				res);
 
 		// load large resource icons
-		add(Type.RESOURCE, hash(Hexagon.Type.LUMBER), R.drawable.res_lumber,
+		add(Type.RESOURCE, Hexagon.Type.LUMBER.ordinal(),
+				R.drawable.res_lumber, res);
+		add(Type.RESOURCE, Hexagon.Type.WOOL.ordinal(), R.drawable.res_wool,
 				res);
-		add(Type.RESOURCE, hash(Hexagon.Type.WOOL), R.drawable.res_wool, res);
-		add(Type.RESOURCE, hash(Hexagon.Type.GRAIN), R.drawable.res_grain, res);
-		add(Type.RESOURCE, hash(Hexagon.Type.BRICK), R.drawable.res_brick, res);
-		add(Type.RESOURCE, hash(Hexagon.Type.ORE), R.drawable.res_ore, res);
-		add(Type.RESOURCE, hash(Hexagon.Type.ANY), R.drawable.trader_any, res);
+		add(Type.RESOURCE, Hexagon.Type.GRAIN.ordinal(), R.drawable.res_grain,
+				res);
+		add(Type.RESOURCE, Hexagon.Type.BRICK.ordinal(), R.drawable.res_brick,
+				res);
+		add(Type.RESOURCE, Hexagon.Type.ORE.ordinal(), R.drawable.res_ore, res);
+		add(Type.RESOURCE, Hexagon.Type.ANY.ordinal(), R.drawable.trader_any,
+				res);
 
 		// load large trader textures
-		add(Type.TRADER, hash(Trader.Position.NORTH), R.drawable.trader_north,
-				res);
-		add(Type.TRADER, hash(Trader.Position.SOUTH), R.drawable.trader_south,
-				res);
-		add(Type.TRADER, hash(Trader.Position.NORTHEAST),
+		add(Type.TRADER, Trader.Position.NORTH.ordinal(),
+				R.drawable.trader_north, res);
+		add(Type.TRADER, Trader.Position.SOUTH.ordinal(),
+				R.drawable.trader_south, res);
+		add(Type.TRADER, Trader.Position.NORTHEAST.ordinal(),
 				R.drawable.trader_northeast, res);
-		add(Type.TRADER, hash(Trader.Position.NORTHWEST),
+		add(Type.TRADER, Trader.Position.NORTHWEST.ordinal(),
 				R.drawable.trader_northwest, res);
-		add(Type.TRADER, hash(Trader.Position.SOUTHEAST),
+		add(Type.TRADER, Trader.Position.SOUTHEAST.ordinal(),
 				R.drawable.trader_southeast, res);
-		add(Type.TRADER, hash(Trader.Position.SOUTHWEST),
+		add(Type.TRADER, Trader.Position.SOUTHWEST.ordinal(),
 				R.drawable.trader_southwest, res);
 
 		// load corner ornaments
-		add(Type.ORNAMENT, hash(Location.BOTTOM_LEFT), R.drawable.bl_corner,
+		add(Type.ORNAMENT, Location.BOTTOM_LEFT.ordinal(),
+				R.drawable.bl_corner, res);
+		add(Type.ORNAMENT, Location.TOP_LEFT.ordinal(), R.drawable.tl_corner,
 				res);
-		add(Type.ORNAMENT, hash(Location.TOP_LEFT), R.drawable.tl_corner, res);
-		add(Type.ORNAMENT, hash(Location.TOP_RIGHT), R.drawable.tr_corner, res);
+		add(Type.ORNAMENT, Location.TOP_RIGHT.ordinal(), R.drawable.tr_corner,
+				res);
 	}
 
 	// private static void shorten(int[] points, double factor) {
@@ -246,7 +222,7 @@ public class TextureManager {
 	// }
 
 	public void draw(Location location, int x, int y, GL10 gl) {
-		Bitmap image = get(Type.ORNAMENT, hash(location));
+		Bitmap image = get(Type.ORNAMENT, location.ordinal());
 
 		int dx = x;
 		int dy = y;
@@ -260,7 +236,7 @@ public class TextureManager {
 
 		gl.glPushMatrix();
 		gl.glTranslatef(dx, dy, 0);
-		square.get(hash(location)).render(gl);
+		square.get(location.ordinal()).render(gl);
 		gl.glPopMatrix();
 	}
 
@@ -269,29 +245,31 @@ public class TextureManager {
 
 		int id = hexagon.getId();
 		gl.glTranslatef(geometry.getHexagonX(id), geometry.getHexagonY(id), 0);
-		square.get(hash(Type.TILE, hash(Hexagon.Type.SHORE))).render(gl);
+		square.get(hash(Type.SHORE, 0)).render(gl);
+		square.get(hash(Type.TILE, hexagon.getType().ordinal())).render(gl);
 
 		gl.glPopMatrix();
 	}
 
-	public void draw(Hexagon hexagon, boolean robber, GL10 gl,
-			Geometry geometry, int lastRoll) {
+	public void draw(Hexagon hexagon, GL10 gl, Geometry geometry, int lastRoll) {
 		gl.glPushMatrix();
 
 		int id = hexagon.getId();
 		gl.glTranslatef(geometry.getHexagonX(id), geometry.getHexagonY(id), 0);
 
-		square.get(hash(Type.TILE, hash(hexagon.getType()))).render(gl);
+		square.get(hash(Type.SHORE, 0)).render(gl);
+		square.get(hash(Type.TILE, hexagon.getType().ordinal())).render(gl);
 
 		int roll = hexagon.getRoll();
 
 		if (hexagon.hasRobber())
 			square.get(hash(Type.ROBBER, 0)).render(gl);
+		
 		else if (lastRoll != 0 && roll == lastRoll)
-			square.get(hash(Type.TILE, hash(Hexagon.Type.LIGHT))).render(gl);
+			square.get(hash(Type.LIGHT, 0)).render(gl);
 
-		// if (roll != 0)
-		// square.get(hash(Type.ROLL, roll)).render(gl);
+		if (roll != 0 && roll != 7)
+			square.get(hash(Type.ROLL, roll)).render(gl);
 
 		gl.glPopMatrix();
 	}
@@ -302,14 +280,15 @@ public class TextureManager {
 		// draw shore access notches
 		gl.glPushMatrix();
 		gl.glTranslatef(geometry.getTraderX(id), geometry.getTraderY(id), 0);
-		square.get(hash(Type.TRADER, hash(trader.getPosition()))).render(gl);
+		square.get(hash(Type.TRADER, trader.getPosition().ordinal()))
+				.render(gl);
 		gl.glPopMatrix();
 
 		// draw type icon
 		gl.glPushMatrix();
 		gl.glTranslatef((float) geometry.getTraderIconOffsetX(id),
 				(float) geometry.getTraderIconOffsetY(id), 0);
-		square.get(hash(Type.RESOURCE, hash(trader.getType()))).render(gl);
+		square.get(hash(Type.RESOURCE, trader.getType().ordinal())).render(gl);
 		gl.glPopMatrix();
 	}
 
@@ -380,7 +359,7 @@ public class TextureManager {
 		else
 			color = Player.Color.NONE;
 
-		Square object = square.get(hash(type, hash(color)));
+		Square object = square.get(hash(type, color.ordinal()));
 		if (square != null) {
 			gl.glPushMatrix();
 			int id = vertex.getIndex();
@@ -391,11 +370,11 @@ public class TextureManager {
 	}
 
 	public Bitmap get(UIButton.Type type) {
-		return get(Type.BUTTON, hash(type));
+		return get(Type.BUTTON, type.ordinal());
 	}
 
 	public Bitmap get(Hexagon.Type type) {
-		return get(Type.RESOURCE, hash(type));
+		return get(Type.RESOURCE, type.ordinal());
 	}
 
 	public void initGL(GL10 gl) {
