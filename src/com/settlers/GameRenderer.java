39a58b801bc48429bf318518ce945e7377bf9182
Neil Isaac <android@neilisaac.ca>
2011-08-22 07:32:55 -0700

    Nicer texture blending

--- src/com/settlers/GameRenderer.java
+++ src/com/settlers/GameRenderer.java
@@ -306,8 +306,8 @@ public class GameRenderer implements Renderer {
 		gl.glEnable(GL10.GL_TEXTURE_2D);
 
 		gl.glEnable(GL10.GL_BLEND);
-		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-		
+		gl.glBlendFunc(GL10.GL_DST_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+
 		texture.initGL(gl);
 	}
 
