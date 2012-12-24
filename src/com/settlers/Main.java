b762ce3b1c69b08c7cdfb0bce0a8eeccec2334a7
Neil Isaac <android@neilisaac.ca>
2012-12-23 19:26:16 -0500

    Switch to using list view for main menu activity

--- src/com/settlers/Main.java
+++ src/com/settlers/Main.java
@@ -1,65 +1,86 @@
 package com.settlers;
 
+import java.util.Vector;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Main extends Activity {
 
 	private static final String DONATE_URL = "https://www.paypal.com/cgi-bin/"
 			+ "webscr?cmd=_donations&business=isaac.neil@gmail.com&"
 			+ "item_name=Island+Settlers+donation&no_shipping=1";
+	
+	private Vector<Runnable> actions; 
 
 	@Override
-	public void onCreate(Bundle state) {
-		super.onCreate(state);
+	public void onResume() {
+		super.onResume();
 
-		if (state == null) {
-			// we were just launched
-		} else {
-			// we are being restored
+		Board board = ((Settlers) getApplicationContext()).getBoardInstance();
+		Settings settings = ((Settlers) getApplicationContext())
+				.getSettingsInstance();
+
+		String events = settings.get("game_events");
+		
+		Vector<String> labels = new Vector<String>();
+		actions = new Vector<Runnable>();
+		
+		if (board != null && board.getWinner(settings) == null) {
+			labels.add(getString(R.string.resume_button));
+			actions.add(new Runnable() {
+				@Override
+				public void run() {
+					Main.this.startActivity(new Intent(Main.this, GameActivity.class));
+				}
+			});
+		} else if (board == null && events != null && events != "") {
+			labels.add(getString(R.string.load_button));
+			actions.add(new Runnable() {
+				@Override
+				public void run() {
+					Main.this.startActivity(new Intent(Main.this, GameActivity.class));
+				}
+			});
 		}
-
-		setContentView(R.layout.main);
-
-		final Button newgame = (Button) findViewById(R.id.newgame);
-		newgame.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
+		
+		labels.add(getString(R.string.new_button));
+		actions.add(new Runnable() {
+			@Override
+			public void run() {
 				Main.this.startActivity(new Intent(Main.this, LocalGame.class));
 			}
 		});
-
-		final Button resume = (Button) findViewById(R.id.resume);
-		resume.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				Main.this.startActivity(new Intent(Main.this, GameActivity.class));
-			}
-		});
-
-		final Button stats = (Button) findViewById(R.id.stats);
-		stats.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
+		
+		labels.add(getString(R.string.stats));
+		actions.add(new Runnable() {
+			@Override
+			public void run() {
 				Main.this.startActivity(new Intent(Main.this, Stats.class));
 			}
 		});
-
-		final Button rules = (Button) findViewById(R.id.rules_button);
-		rules.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
+		
+		labels.add(getString(R.string.rules_button));
+		actions.add(new Runnable() {
+			@Override
+			public void run() {
 				Main.this.startActivity(new Intent(Main.this, Rules.class));
 			}
 		});
 
-		final Button about = (Button) findViewById(R.id.about);
-		about.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
+		labels.add(getString(R.string.about_button));
+		actions.add(new Runnable() {
+			@Override
+			public void run() {
 				final Builder aboutDialog = new AlertDialog.Builder(Main.this);
 				aboutDialog.setTitle(R.string.app_name);
 				aboutDialog.setIcon(R.drawable.icon);
@@ -67,7 +88,6 @@ public class Main extends Activity {
 						+ getString(R.string.acknowledgements) + "\n\n"
 						+ getString(R.string.translators));
 
-				// aboutDialog.setNegativeButton(R.string.dismiss_button, null);
 				aboutDialog.setPositiveButton(R.string.donate_button,
 						new DialogInterface.OnClickListener() {
 							@Override
@@ -91,26 +111,22 @@ public class Main extends Activity {
 				aboutDialog.show();
 			}
 		});
+		
+		String[] values = new String[labels.size()];
+		for (int i = 0; i < values.length; i++)
+			values[i] = labels.get(i);
+        
+        ListView view = new ListView(this);
+        
+        view.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values));
+        
+        view.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				actions.get(position).run();
+			}
+		});
+        
+        setContentView(view);
 	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
-		final Button resume = (Button) findViewById(R.id.resume);
-
-		Board board = ((Settlers) getApplicationContext()).getBoardInstance();
-		Settings settings = ((Settlers) getApplicationContext())
-				.getSettingsInstance();
-
-		String events = settings.get("game_events");
-
-		if (board != null && board.getWinner(settings) == null) {
-			resume.setEnabled(true);
-		} else if (board == null && events != null && events != "") {
-			resume.setEnabled(true);
-			resume.setText(getString(R.string.load_button));
-		}
-	}
-
 }
