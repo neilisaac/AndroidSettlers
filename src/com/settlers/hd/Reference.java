239c044c711ecd38fc98f78711a3236156463952
Neil Isaac <android@neilisaac.ca>
2013-03-03 14:01:57 -0500

    Use ViewPager for quick reference window

--- src/com/settlers/hd/Reference.java
+++ src/com/settlers/hd/Reference.java
@@ -1,37 +1,68 @@
 package com.settlers.hd;
 
-import android.app.TabActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-@SuppressWarnings("deprecation")
-public class Reference extends TabActivity {
+public class Reference extends Activity {
 
-	private static final int[] CONTENT = { R.id.ref_content1,
-			R.id.ref_content2, R.id.ref_content3 };
+	private static final int[] LAYOUTS = { R.layout.reference_build, R.layout.reference_buy,
+			R.layout.reference_development };
 
-	private static final int[] NAMES = { R.string.reference_tab1,
-			R.string.reference_tab2, R.string.reference_tab3 };
+	private static final int[] NAMES = { R.string.reference_tab1, R.string.reference_tab2, R.string.reference_tab3 };
+
+	private View[] views;
 
 	@Override
 	public void onCreate(Bundle state) {
 		super.onCreate(state);
+
 		setContentView(R.layout.reference);
-		TabHost host = getTabHost();
-		host.setup();
+		setTitle(getString(R.string.reference));
 
-		setTitle(getString(R.string.app_name) + " "
-				+ getString(R.string.reference));
+		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-		for (int i = 0; i < NAMES.length; i++) {
-			String name = getString(NAMES[i]);
-			TabSpec spec = host.newTabSpec(name);
-			spec.setIndicator(name);
-			spec.setContent(CONTENT[i]);
-			host.addTab(spec);
+		views = new View[LAYOUTS.length];
+		
+		for (int i = 0; i < LAYOUTS.length; i++)
+			views[i] = inflater.inflate(LAYOUTS[i], null);
+		
+		ViewPager viewPager = (ViewPager) findViewById(R.id.reference);
+		viewPager.setAdapter(new ReferenceTabAdapter());
+		viewPager.setCurrentItem(1);
+	}
+
+	public class ReferenceTabAdapter extends PagerAdapter {
+
+		@Override
+		public int getCount() {
+			return views.length;
 		}
 
-		host.setCurrentTab(0);
+		@Override
+		public boolean isViewFromObject(View view, Object object) {
+			return view == object;
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup collection, int position) {
+			collection.addView(views[position]);
+			return views[position];
+		}
+
+		@Override
+		public void destroyItem(ViewGroup collection, int position, Object view) {
+			collection.removeView((View) view);
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return getString(NAMES[position]);
+		}
 	}
 }
